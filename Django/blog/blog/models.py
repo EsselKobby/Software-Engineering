12d5f1f948fe307032a05594379074a8c69d9716
EsselKobby <essel.bernard.kobby@gmail.com>
2024-11-12 11:44:40 +0000

    aiBlogGenerator directory and virtual environment

--- Django/blog/blog/models.py
+++ Django/blog/blog/models.py
@@ -11,3 +11,25 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+class Artist(models.Model):
+    name= models.CharField(max_length=200, db_index=True, help_text='Artist name')
+
+    def __str__(self):
+        """String for representing the Model object"""
+        return self.name
+
+class Album(models.Model):
+    title= models.CharField(max_length=200, db_index=True, help_text='Album title')
+    artist= models.ForeignKey('Artist', on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        """String for representing the Model object"""
+        return self.title
+
+class Genre(models.Model):
+    name= models.CharField(max_length=200, db_index=True, help_text='Genre of music (i.e. Blues)')
+
+    def __str__(self):
+        """String for representing the Model object"""
+        return self.name
