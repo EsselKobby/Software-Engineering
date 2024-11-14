41f8517e7bf9263da948a4bf2dd9ed5d47a64e7c
EsselKobby <essel.bernard.kobby@gmail.com>
2024-11-14 13:01:34 +0000

    Backend Update

--- Django/AiBlogGenerator/BACKEND/ai_blog_app/urls.py
+++ Django/AiBlogGenerator/BACKEND/ai_blog_app/urls.py
@@ -1,7 +1,8 @@
-"""ai_blog_app URL Configuration
+"""
+URL configuration for ai_blog_app project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.1/topics/http/urls/
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -18,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blog_generator.urls'))
+    path('', include('blog_generator.urls')),
 ]
