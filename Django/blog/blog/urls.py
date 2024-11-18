23dc3b3ea3bcf59d7a5fb4d5f6f4297cce2d7100
EsselKobby <essel.bernard.kobby@gmail.com>
2024-11-18 13:44:24 +0000

    BlogDetaIlView and template update

--- Django/blog/blog/urls.py
+++ Django/blog/blog/urls.py
@@ -1,8 +1,9 @@
 # blogs/urls.py
 
 from django.urls import path
-from .views import BlogListView
+from .views import BlogListView, BlogDetailView
 
 urlpatterns = [
+    path('post/<int:pk>/', BlogDetailView.as_view(), name='post_detail'), #new
     path('', BlogListView.as_view(), name='home'),
 ]
--- Django/blog/blog/views.py
+++ Django/blog/blog/views.py
@@ -8,3 +8,7 @@ from .models import Post
 class BlogListView(ListView):
     model = Post
     template_name = 'home.html'
+    
+class BlogDetailView(ListView):
+    model = Post
+    template_name = 'post_detail.html'
