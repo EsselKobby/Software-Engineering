ca886a9f2ef65505677f2e994f1d9add8a2d98c1
EsselKobby <essel.bernard.kobby@gmail.com>
2024-11-26 14:38:29 +0000

    new email field in the models.py

--- Django/news/newspaper_project/settings.py
+++ Django/news/newspaper_project/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'users.apps.UsersConfig', #new
+    'pages.apps.PagesConfig', #new
 ]
 
 AUTH_USER_MODEL = 'users.CustomUser' #new
